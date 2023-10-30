cff5fd16836dc2f2910125931490272152e80606
ROHETH-S <roheth1908@gmail.com>
2023-10-30 16:28:01 +0530

    updated3

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,8 @@ app.use(express.json());
 
 app.use(
     cors({
-        origin:'http://localhost:5173',
+        // origin:'http://localhost:5173',
+        origin:'https://book-store-management.vercel.app',
         methods:['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders:['Content-Type'],
     })
